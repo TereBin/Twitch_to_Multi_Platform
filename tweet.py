6dbf08f38f3fcc96660ca3ab0c7c80bcd828f22c
TereBin <44892691+TereBin@users.noreply.github.com>
2022-07-21 13:44:13 +0900

    Update tweet.py

--- tweet.py
+++ tweet.py
@@ -1,23 +1,20 @@
 import tweepy
 
-twitter_data_txt = open("D:/TereBin/TtTB/twitter_api_data.txt", 'r')
-twitter_data = twitter_data_txt.read().splitlines()
-api_key = twitter_data[0]
-api_secret = twitter_data[1]
-access_token = twitter_data[2]
-access_secret = twitter_data[3]
-twitter_data_txt.close()
+def tweet(twitter_api_data_path, tweet, link):
+    twitter_data_txt = open(twitter_api_data_path, 'r')
+    twitter_data = twitter_data_txt.read().splitlines()
+    api_key = twitter_data[0]
+    api_secret = twitter_data[1]
+    access_token = twitter_data[2]
+    access_secret = twitter_data[3]
+    twitter_data_txt.close()
+    tweet = tweet + "\n" + link
 
-print(api_key)
-print(api_secret)
-print(access_token)
-print(access_secret)
-def send_tweet(tweet, api_key, api_secret, access_token, access_secret):
-    auth = tweepy.OAuthHandler(api_key, api_secret)
-    auth.set_access_token(access_token, access_secret)
-    bot = tweepy.API(auth)
-    bot.update_status(status = tweet)
+    def send_tweet(tweet, api_key, api_secret, access_token, access_secret):
+        auth = tweepy.OAuthHandler(api_key, api_secret)
+        auth.set_access_token(access_token, access_secret)
+        bot = tweepy.API(auth)
+        bot.update_status(status=tweet)
 
-tweet = "API 활용 봇 트윗 테스트입니다"
-
-send_tweet(tweet, api_key, api_secret, access_token, access_secret)
+    print(tweet)
+    send_tweet(tweet, api_key, api_secret, access_token, access_secret)
