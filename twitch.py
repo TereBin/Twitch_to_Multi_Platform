44d1061c7a9f15dc53dc009ec0f2a994ee26f150
TereBin <44892691+TereBin@users.noreply.github.com>
2022-07-21 16:25:23 +0900

    Update twitch.py

--- twitch.py
+++ twitch.py
@@ -1,36 +1,27 @@
 import requests
 
-streamer_ID = 'terebin_420'
-# get app_key and app_secret from local txt file
-app_data_txt = open("D:/TereBin/TtTB/twitch_app_data.txt", 'r')
-app_data = app_data_txt.read().splitlines()
-app_key = app_data[0]
-app_secret = app_data[1]
-app_data_txt.close()
+# get channel data from twitch. get it every minute
+def check_twitch(streamer_id, app_key, auth_token):
+    stream_headers = {'client-id': app_key, 'Authorization': auth_token}
+    stream_req = requests.get(f"https://api.twitch.tv/helix/search/channels?query={streamer_id}", headers=stream_headers)
+    stream_data_json = stream_req.json()["data"]
 
-# get access_token and token_type from oauth2
-auth_req = requests.post('https://id.twitch.tv/oauth2/token?client_id=' + app_key + '&client_secret=' + app_secret + '&grant_type=client_credentials')
-auth_req_json = auth_req.json()
+    # check for specific streamer
+    i = 0
+    is_live = False  # refreshing variable is_live
+    while i < len(stream_data_json):
+        stream_data = stream_data_json[i]
+        if stream_data["broadcaster_login"] == streamer_id:
+            is_live = stream_data["is_live"]
+            print(stream_data["display_name"])
+            break
+        else:
+            i += 1
 
-access_token = auth_req_json["access_token"]
-token_type = auth_req_json["token_type"]
-token_type = token_type[0].upper() + token_type[1:]
+    if is_live:
+        print("online")
+        return is_live, i
 
-auth_token = token_type+" "+access_token
-# print(auth_token)
-headers = {'client-id':app_key, 'Authorization':auth_token}
-
-# get channel data from twitch
-stream_req = requests.get(f"https://api.twitch.tv/helix/search/channels?query={streamer_ID}", headers=headers)
-stream_req_json = stream_req.json()
-
-# check for specific streamer
-i = 0
-while i < len(stream_req_json["data"]):
-    if stream_req_json["data"][i]["broadcaster_login"] == streamer_ID :
-        is_live = stream_req_json["data"][i]["is_live"]
-        break
-    else :
-        i+=1
-
-print(is_live)
+    else:
+        print("offline")
+        return is_live, None
