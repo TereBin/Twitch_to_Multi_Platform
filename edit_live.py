7d8a5280dc3b7d9204cab7535745a64317bd73db
TereBin <44892691+TereBin@users.noreply.github.com>
2022-07-21 16:25:36 +0900

    Update edit_live.py

--- edit_live.py
+++ edit_live.py
@@ -2,8 +2,18 @@ import json
 
 streamer_json_path = "D:/TereBin/TtTB/streamer_list.json"
 live_stream = 1
-def edit_list(streamer_json_path, live_stream):
-    streamer_json = json.load(open(streamer_json_path, 'r', encoding='utf-8'))  # open streamer_list.json as streamer_json
-    streamer_json[str(live_stream)]["4_signal"] = "False"
-    with open(streamer_json_path, 'w') as file:
-        json.dump(streamer_json, file, indent="\t")
+
+def edit_live(streamer_json_path, is_live, live_stream):
+    streamer_json = json.load(
+        open(streamer_json_path, 'r', encoding='utf-8'))  # open streamer_list.json as streamer_json
+    file = json.load(open(streamer_json_path, 'w', encoding='utf-8'))
+    if is_live == "True" and streamer_json[str(live_stream)]["4_signal"] == "False":
+        streamer_json[str(live_stream)]["4_signal"] = "True"
+        json.dumps(streamer_json, file, indent="\t")
+        return True
+    elif is_live == "False" and streamer_json[str(live_stream)]["4_signal"] == "True":
+        streamer_json[str(live_stream)]["4_signal"] = "False"
+        json.dumps(streamer_json, file, indent="\t")
+        return None
+    else:
+        return None
