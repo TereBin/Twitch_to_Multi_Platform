aa9cdeb3d8a18f76e7ad3fd370bfb1e98b048731
TereBin <44892691+TereBin@users.noreply.github.com>
2022-07-29 02:41:16 +0900

    Update tweet.py

--- tweet.py
+++ tweet.py
@@ -1,14 +1,15 @@
 import tweepy
 
-def tweet(twitter_api_data_path, tweet, link):
+def tweet(twitter_api_data_path, streamer_data, game_name, title, link):
     twitter_data_txt = open(twitter_api_data_path, 'r')
     twitter_data = twitter_data_txt.read().splitlines()
     api_key = twitter_data[0]
     api_secret = twitter_data[1]
-    access_token = twitter_data[2]
-    access_secret = twitter_data[3]
     twitter_data_txt.close()
-    tweet = tweet + "\n" + link
+    tweet = streamer_data["3_tweet"]
+    access_token = streamer_data["5_access_token"]
+    access_secret = streamer_data["6_access_secret"]
+    tweet = tweet + "\n" + "게임 : " + game_name + "\n" + "방제 : " + "[" + title + "]\n" + link
 
     def send_tweet(tweet, api_key, api_secret, access_token, access_secret):
         auth = tweepy.OAuthHandler(api_key, api_secret)
