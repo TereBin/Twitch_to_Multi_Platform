6b48a563228cde4f2a724782b3800e7074b631ad
TereBin <44892691+TereBin@users.noreply.github.com>
2022-08-02 21:05:21 +0900

    Update edit_list.py

--- subcode/edit_list.py
+++ subcode/edit_list.py
@@ -1,24 +1,31 @@
-import json
-import pprint as pp
+import requests
 
-streamer_json_path = "D:/TereBin/TtTB/data/streamer_list.json"
+# get channel data from twitch. get it every minute
+def check_twitch(streamer_id, app_key, auth_token):
+    stream_headers = {'client-id': app_key, 'Authorization': auth_token}
+    stream_req = requests.get(f"https://api.twitch.tv/helix/search/channels?query={streamer_id}", headers=stream_headers)
+    stream_data_json = stream_req.json()["data"]
 
-def edit_list(i, is_live):
-    streamer_json = json.load(
-        open(streamer_json_path, 'r', encoding='utf-8'))  # readable
-    pp.pprint(streamer_json[str(i)])
-    if is_live and streamer_json[str(i)]["4_signal"] == "False":
-        streamer_json[str(i)]["4_signal"] = "True"
-        with open(streamer_json_path, 'w', encoding='utf-8') as file:
-            json.dump(streamer_json, file, indent="\t")
-        print("방송시작\n")
-        return True
-    if (is_live == False) and streamer_json[str(i)]["4_signal"] == "True":
-        streamer_json[str(i)]["4_signal"] = "False"
-        with open(streamer_json_path, 'w', encoding='utf-8') as file:
-            json.dump(streamer_json, file, indent="\t")
-        print("방송종료\n")
-        return False
+    # check for specific streamer
+    i = 0
+    category = ""
+    title = ""
+    is_live = False  # refreshing variable is_live
+    while i < len(stream_data_json):
+        stream_data = stream_data_json[i]
+        if stream_data["broadcaster_login"] == streamer_id:
+            is_live = stream_data["is_live"]
+            category = stream_data["game_name"]
+            title = stream_data["title"]
+            print(stream_data["display_name"])
+            break
+        else:
+            i += 1
+
+    if is_live:
+        print("online")
+        return is_live, category, title
+
     else:
-        print("현상유지\n")
-        return False
+        print("offline")
+        return is_live, category, title
