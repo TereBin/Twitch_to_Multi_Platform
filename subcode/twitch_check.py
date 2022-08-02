77d623cd6efa3494c1a13e535898f715044458bf
TereBin <44892691+TereBin@users.noreply.github.com>
2022-08-02 21:05:04 +0900

    Update twitch_check.py

--- subcode/twitch_check.py
+++ subcode/twitch_check.py
@@ -8,12 +8,14 @@ def check_twitch(streamer_id, app_key, auth_token):
 
     # check for specific streamer
     i = 0
+    category = ""
+    title = ""
     is_live = False  # refreshing variable is_live
     while i < len(stream_data_json):
         stream_data = stream_data_json[i]
         if stream_data["broadcaster_login"] == streamer_id:
             is_live = stream_data["is_live"]
-            game_name = stream_data["game_name"]
+            category = stream_data["game_name"]
             title = stream_data["title"]
             print(stream_data["display_name"])
             break
@@ -22,8 +24,8 @@ def check_twitch(streamer_id, app_key, auth_token):
 
     if is_live:
         print("online")
-        return is_live, game_name, title
+        return is_live, category, title
 
     else:
         print("offline")
-        return is_live, game_name, title
+        return is_live, category, title
