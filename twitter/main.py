439d7bd43aed718a395751a23e90923ac90ebdb8
TereBin <44892691+TereBin@users.noreply.github.com>
2022-10-24 00:30:53 +0900

    Update main.py

--- twitter/main.py
+++ twitter/main.py
@@ -13,6 +13,7 @@ streamer_json_path = "data/streamer_list.json"
 app_data_path = "data/twitch_app_data.txt"
 twitter_api_data_path = "data/twitter_api_data.txt"
 twitch_bot_data_path = 'data/twitch_bot_data.txt'
+err_log_path = 'data/err_log.txt'
 
 # get twitch api key
 app_data_txt = open(app_data_path, 'r')
@@ -50,9 +51,14 @@ while True:
                     tweet(twitter_api_data_path, streamer_data, category, title, "twitch.tv/" + streamer_data["1_twitch_id"])
                     send_chat(twitch_bot_data_path, streamer_data["1_twitch_id"])
                     print("트윗 전송\n")
-                except tweepy.errors.Forbidden as e:
+                except tweepy.errors.Forbidden as err:
+                    err_str = str(err)
+                    with open(err_log_path) as f:
+                        err_code = "[" + str(
+                            time.strftime('%m/%d %H:%M', time.localtime(time.time()))) + "] " + "tweepy error : \n" + err_str + "\n\n"
+                        f.write(err_code)
                     print("tweepy error")
-                    print("사유 :", e, "\n")
+                    print("사유 :", err, "\n")
                     pass
         i += 1
     print("실행시간 :", str(round(time.time() - start, 2)) + "초")
