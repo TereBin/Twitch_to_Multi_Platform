66f48772c888c658bcb83e000f46b61b3bb939e3
TereBin <44892691+TereBin@users.noreply.github.com>
2022-12-15 21:02:35 +0900

    Update main.py

--- twitter/main.py
+++ twitter/main.py
@@ -12,8 +12,8 @@ from send_chat import send_chat
 streamer_json_path = "data/streamer_list.json"
 app_data_path = "data/twitch_app_data.txt"
 twitter_api_data_path = "data/twitter_api_data.txt"
+err_log_path = "data/err_log.txt"
 twitch_bot_data_path = 'data/twitch_bot_data.txt'
-err_log_path = 'data/err_log.txt'
 
 # get twitch api key
 app_data_txt = open(app_data_path, 'r')
@@ -35,32 +35,55 @@ auth_token = token_type + " " + access_token
 while True:
     start = time.time()
     print(time.strftime('%y/%m/%d %H:%M', time.localtime(time.time())), "\n")
-    streamer_dict = read_list(streamer_json_path)
+    try :
+        streamer_dict = read_list(streamer_json_path)
     
-    i = 1  # 0 is sample data
-    while i < len(streamer_dict):
-        streamer_data = streamer_dict[str(i)]
-        is_live, category, title = check_twitch(streamer_data["1_twitch_id"], app_key, auth_token)
-        if is_live is None:
-            print("")
-        else:
-            live_change = edit_list(i, is_live)
+        i = 1  # 0 is sample data
+        while i < len(streamer_dict):
+            streamer_data = streamer_dict[str(i)]
+            is_live, category, title = check_twitch(streamer_data["1_twitch_id"], app_key, auth_token)
+            if is_live is None:
+                print("")
+            else:
+                live_change = edit_list(i, is_live)
 
-            if live_change:
-                try:
-                    tweet(twitter_api_data_path, streamer_data, category, title, "twitch.tv/" + streamer_data["1_twitch_id"])
-                    send_chat(twitch_bot_data_path, streamer_data["1_twitch_id"])
-                    print("트윗 전송\n")
-                except tweepy.errors.Forbidden as err:
-                    err_str = str(err)
-                    with open(err_log_path) as f:
-                        err_code = "[" + str(
-                            time.strftime('%m/%d %H:%M', time.localtime(time.time()))) + "] " + "tweepy error : \n" + err_str + "\n\n"
-                        f.write(err_code)
-                    print("tweepy error")
-                    print("사유 :", err, "\n")
-                    pass
-        i += 1
-    print("실행시간 :", str(round(time.time() - start, 2)) + "초")
-    print("-"*50)
-    time.sleep(60)
+                if live_change:
+                    try:
+                        tweet(twitter_api_data_path, streamer_data, category, title, "twitch.tv/" + streamer_data["1_twitch_id"])
+                        send_chat(twitch_bot_data_path, streamer_data["1_twitch_id"])
+                        print("트윗 전송\n")
+                    except tweepy.errors.Forbidden as err:
+                        err_str = str(err)
+                        with open(err_log_path, 'a') as f:
+                            err_code = "[" + str(time.strftime('%m/%d %H:%M', time.localtime(time.time()))) + "] " + "tweepy error : \n" + err_str + "\n\n"
+                            f.write(err_code)
+                        print("tweepy error")
+                        print("사유 :", err, "\n")
+                        pass
+            i += 1
+        run_time = round(time.time() - start, 2)
+        print("실행시간 :", str(run_time) + "초")
+        print("-"*50)
+        if run_time < 60:
+            time.sleep(60 - run_time)
+        else:
+            time.sleep(60 - (run_time % 60))
+            time.sleep(0)
+            
+    except Exception as err:
+        err_str = str(err)
+        with open(err_log_path, 'a') as f:
+            err_code = "[" + str(time.strftime('%m/%d %H:%M', time.localtime(time.time()))) + "] " + "new error : \n" + err_str + "\n\n"
+            f.write(err_code)
+        print("new error")
+        print("사유 :", err, "\n")
+        
+        run_time = round(time.time() - start, 2)
+        print("실행시간 :", str(run_time) + "초")
+        print("-"*50)
+        if run_time < 60:
+            time.sleep(60 - run_time)
+        else:
+            time.sleep(60 - (run_time % 60))
+            time.sleep(0)
+        pass
